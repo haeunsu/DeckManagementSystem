bac6dc862e95a47b27c2058d82f9d49ff1a99fa6
haeun <haeun@dongchimy>
2023-03-23 20:17:33 +0900

    Building a Loop

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -1,13 +1,21 @@
+import java.util.Scanner;
 
 public class MenuManager {
 	public static void main(String[] args) {
+		int num = 0;
+		Scanner input = new Scanner(System.in);
 		
+		while(num != 6) {
+		System.out.println("*** Deck Management System Menu ***");
 		System.out.println("1. Add Decks");
 		System.out.println("2. Delete Decks");
 		System.out.println("3. Edit Decks");
 		System.out.println("4. View Decks");
 		System.out.println("5. Show a Menu");
 		System.out.println("6. Exit");
+		System.out.println("Select one number between 1-6: ");
+		num = input.nextInt();
+		}
 	}
 
 }
