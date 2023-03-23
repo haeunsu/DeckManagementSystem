7c5b98e31333e796952025f8d0b605d3990d67d9
haeun <haeun@dongchimy>
2023-03-23 21:02:27 +0900

    fin

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -15,6 +15,25 @@ public class MenuManager {
 		System.out.println("6. Exit");
 		System.out.println("Select one number between 1-6: ");
 		num = input.nextInt();
+		switch(num) {
+		case 1:
+			input.nextLine();//여기서 버퍼를 비워준다는게 뭔뜻이지
+			System.out.print("Deck Name: ");
+			String deckname1 = input.nextLine();
+			System.out.print("Deck Price: ");
+			int deckprice = input.nextInt();
+			System.out.print("Deck Type: ");
+			String decktype = input.next();
+			System.out.print("Avarage Mana Cost: ");
+			double manacost = input.nextDouble();
+			break;
+		case 2:
+		case 3:
+		case 4:
+			input.nextLine();
+			System.out.print("Deck Name: ");
+			String deckname2 = input.nextLine();
+		}
 		}
 	}
 
