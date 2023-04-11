86dcac716cc2e29331ec86039319b0c13dadc09e
haeun <haeun@dongchimy>
2023-04-11 20:52:31 +0900

    full change

--- src/helloworld/Hiyo.java
+++ src/helloworld/Hiyo.java
@@ -4,7 +4,7 @@ public class Hiyo {
 public static void main(String[] args) {
 		
 		System.out.println("hello World!");
-		System.out.println("bye World");
+		System.out.println("bye World!!");
 	}
 
 }
