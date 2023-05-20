40d452f7a2ba2d4bfaebdf1f1a563bb243d336f4
haeun <haeun@dongchimy>
2023-05-20 13:22:49 +0900

    after refactoring1

--- src/Deckmanager.java
+++ src/Deckmanager.java
@@ -17,7 +17,7 @@ public class Deckmanager {
 	
 	public void addDeck() {
 		int kind = 0;
-		DeckInput DeckInput;
+		DeckInput deckInput;
 		while (kind != 1 && kind !=2 ){
 			System.out.println("1 for Worrior ");
 			System.out.println("2 for Shamman ");
@@ -25,21 +25,21 @@ public class Deckmanager {
 			System.out.print("Select num 1, 2, or 3 for Deck Kind: ");
 			kind = input.nextInt();
 			if (kind == 1) {
-				DeckInput = new WorriorDeck(DeckKind.Warrior);
-				DeckInput.getUserInput(input);
-				decks.add(DeckInput);
+				deckInput = new WorriorDeck(DeckKind.Warrior);
+				deckInput.getUserInput(input);
+				decks.add(deckInput);
 				break;
 			}
 			else if (kind == 2) {
-				DeckInput = new ShamanDeck(DeckKind.Shaman);
-				DeckInput.getUserInput(input);
-				decks.add(DeckInput);
+				deckInput = new ShamanDeck(DeckKind.Shaman);
+				deckInput.getUserInput(input);
+				decks.add(deckInput);
 				break;
 			}
 			else if (kind == 3) {
-				DeckInput = new MageDeck(DeckKind.Mage);
-				DeckInput.getUserInput(input);
-				decks.add(DeckInput);
+				deckInput = new MageDeck(DeckKind.Mage);
+				deckInput.getUserInput(input);
+				decks.add(deckInput);
 				break;
 			}
 			else {
@@ -51,63 +51,61 @@ public class Deckmanager {
 	public void deleteDeck() {
 		System.out.print("deck number: ");
 		int deckNumber = input.nextInt();
+		int index = findIndex(deckNumber);
+		removefromdecks(index, deckNumber);
+	}
+	
+	public int findIndex(int deckNumber){
 		int index = -1;
 		for (int i = 0; i<decks.size(); i++) {
 			if(decks.get(i).getNumber() == deckNumber) {
 				index = i;
 				break;
-			}			
+			}
 		}
-		
+		return index;
+	}
+	
+	public int removefromdecks(int index, int deckNumber) {
 		if (index >=0 ) {
 			decks.remove(index);
 			System.out.println("the deck " + deckNumber + " is deleted");
+			return 1;
 			
 		}
 		else {
 			System.out.println("the deck has not been registered");
-			return;
+			return -1;
 		}
 	}
+	
 	public void editDeck() {
 		System.out.print("deck number: ");
 		int deckNumber = input.nextInt();
 		for (int i = 0; i<decks.size(); i++) {
-			DeckInput deckInput = decks.get(i);
-			if(deckInput.getNumber() == deckNumber) {
+			DeckInput deck = decks.get(i);
+			if(deck.getNumber() == deckNumber) {
 			int num = -1;
 			while(num != 5) {
-				System.out.println("*** Deck Management System Menu ***");
-				System.out.println("1. Add Decks ");
-				System.out.println("2. Delete Decks ");
-				System.out.println("3. Edit Decks ");
-				System.out.println("4. View Decks ");
-				System.out.println("5. Exit ");
-				System.out.println("Select one number between 1-5: ");
+				showEditMenu();
 				num = input.nextInt();
-				if (num==1) {
-					System.out.print(" Deck Number: ");
-					int Number = input.nextInt();
-					deckInput.setNumber(Number);
-				}
-				else if (num==2) {
-					System.out.print(" Deck Name: ");
-					String name = input.next();
-					deckInput.setName(name);
-				}
-				else if (num==3) {
-					System.out.print(" Deck Price: ");
-					int price = input.nextInt();
-					deckInput.setPrice(price);
-				}
-				else if (num==4) {
-					System.out.print(" Deck Type: ");
-					String type = input.next();
-					deckInput.setType(type);
-				}
-				else {
+				switch(num) {
+				case 1:
+					setDeckNumber(deck, input);
+					break;
+				case 2:
+					setDeckName(deck, input);
+					break;
+				case 3:
+					setDeckPrice(deck, input);
+					break;
+				case 4:
+					setDeckType(deck, input);
+					break;
+				default:
 					continue;
-					} // if
+				}
+				
 				} //while
 			break;
 		} //if
@@ -115,12 +113,45 @@ public class Deckmanager {
 		} //for
 	}
 	public void viewDeck() {
-//		System.out.print("deck number");
-//		int deckNumber = input.nextInt();
 		System.out.println("# of registered decks:" + decks.size());
 		for (int i = 0; i<decks.size(); i++) {
 			decks.get(i).printInfo();
 		}
+	}
+	
+	public void setDeckNumber(DeckInput deck, Scanner input) {
+		System.out.print(" Deck Number: ");
+		int Number = input.nextInt();
+		deck.setNumber(Number);
+	}
+	
+	public void setDeckName(DeckInput deck, Scanner input) {
+		System.out.print(" Deck Name: ");
+		String name = input.next();
+		deck.setName(name);
+	}
+	
+	public void setDeckPrice(DeckInput deck, Scanner input) {
+		System.out.print(" Deck Price: ");
+		int price = input.nextInt();
+		deck.setPrice(price);
+	}
+	
+	public void setDeckType(DeckInput deck, Scanner input) {
+		System.out.print(" Deck Type: ");
+		String type = input.next();
+		deck.setType(type);
+	}
+	
+	public void showEditMenu() {
+		System.out.println("*** Deck Management System Menu ***");
+		System.out.println("1. Add Decks ");
+		System.out.println("2. Delete Decks ");
+		System.out.println("3. Edit Decks ");
+		System.out.println("4. View Decks ");
+		System.out.println("5. Exit ");
+		System.out.println("Select one number between 1-5: ");
+		
 	}
 
 }
--- src/MenuManager.java
+++ src/MenuManager.java
@@ -4,9 +4,58 @@ public class MenuManager {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		Deckmanager deckmanager = new Deckmanager(input);
-		
+
 		int num = -1;
-		while(num != 5) {
+		while (num != 5) {
+			showMenu();
+			num = input.nextInt();
+			switch (num) {
+			case 1:
+				deckmanager.addDeck();
+				break;
+			case 2:
+				deckmanager.deleteDeck();
+				break;
+			case 3:
+				deckmanager.editDeck();
+				break;
+			case 4:
+				deckmanager.viewDeck();
+				break;
+			default:
+				continue;
+			}
+
+		}
+	}
+//	public static void addDeck() {
+//		Scanner input = new Scanner(System.in);
+//		System.out.print("deck number ");
+//		int deckNumber = input.nextInt();
+//		System.out.print("deck name ");
+//		String deckName = input.next();
+//		System.out.print("deck price ");
+//		int deckPrice = input.nextInt();
+//		System.out.print("deck type ");
+//		String decktype = input.next();
+//	}
+//	public static void deleteDeck() {
+//		Scanner input = new Scanner(System.in);
+//		System.out.print("deck number ");
+//		int deckNumber = input.nextInt();
+//	}
+//	public static void editDeck() {
+//		Scanner input = new Scanner(System.in);
+//		System.out.print("deck number ");
+//		int deckNumber = input.nextInt();
+//	}
+//	public static void viewDeck() {
+//		Scanner input = new Scanner(System.in);
+//		System.out.print("deck number ");
+//		int deckNumber = input.nextInt();
+//	}
+
+	public static void showMenu() {
 		System.out.println("*** Deck Management System Menu ***");
 		System.out.println("1. Add Decks");
 		System.out.println("2. Delete Decks");
@@ -14,48 +63,6 @@ public class MenuManager {
 		System.out.println("4. View Decks");
 		System.out.println("5. Exit");
 		System.out.println("Select one number between 1-5: ");
-		num = input.nextInt();
-		if (num==1) {
-			deckmanager.addDeck();
-		}
-		else if (num==2) {
-			deckmanager.deleteDeck();
-		}
-		else if (num==3){
-			deckmanager.editDeck();
-		}
-		else if (num==4){
-			deckmanager.viewDeck();
-		}
-		else {
-			continue;
-			}
-		}
 	}
-	public static void addDeck() {
-		Scanner input = new Scanner(System.in);
-		System.out.print("deck number ");
-		int deckNumber = input.nextInt();
-		System.out.print("deck name ");
-		String deckName = input.next();
-		System.out.print("deck price ");
-		int deckPrice = input.nextInt();
-		System.out.print("deck type ");
-		String decktype = input.next();
-	}
-	public static void deleteDeck() {
-		Scanner input = new Scanner(System.in);
-		System.out.print("deck number ");
-		int deckNumber = input.nextInt();
-	}
-	public static void editDeck() {
-		Scanner input = new Scanner(System.in);
-		System.out.print("deck number ");
-		int deckNumber = input.nextInt();
-	}
-	public static void viewDeck() {
-		Scanner input = new Scanner(System.in);
-		System.out.print("deck number ");
-		int deckNumber = input.nextInt();
-	}
+
 }
